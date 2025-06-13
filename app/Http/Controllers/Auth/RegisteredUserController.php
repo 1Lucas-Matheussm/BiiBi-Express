0c8f70bcba8bfedf3941f4fa9d76ad52bf2153d1
1Lucas-Matheussm <1lucasmatheus.silvam@gmail.com>
2025-06-13 19:53:33 -0300

    edite Register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -22,14 +22,34 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'phone' => ['required', 'string', 'max:20'],
+            'document' => ['required', 'string', 'max:20'],
+            'address' => ['required', 'string', 'max:255'],
+            'role' => ['required', 'in:cliente,entregador,empresa'],
+
+            'vehicle_type' => ['nullable', 'required_if:role,entregador', 'string', 'max:20'],
+            'plate' => ['nullable', 'required_if:role,entregador', 'string', 'max:10'],
+            'rating' => ['nullable', 'numeric', 'min:0', 'max:5'],
+            'company_name' => ['nullable', 'required_if:role,empresa', 'string', 'max:255'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->string('password')),
+
+            'phone' => $request->phone,
+            'document' => $request->document,
+            'address' => $request->address,
+            'role' => $request->role,
+            'email_verified' => false,
+
+            'vehicle_type' => $request->vehicle_type,
+            'plate' => $request->plate,
+            'rating' => $request->rating,
+            'company_name' => $request->company_name,
         ]);
 
         event(new Registered($user));
